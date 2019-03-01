3d67442b40eae5b75e40e227fcd7ffa9e60bfcb4
Manish Kumar49 <MK00507138@techmahindra.com>
2019-03-01 18:44:17 +0530

    initial commit

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  database: 'mongodb://localhost:27017/amazonclone',
+  port: 3030,
+  secret: 'manish sharan'
+}
---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose =  require('mongoose');
+const config = require('./config');
+const cors = require('cors');
+
+const app = express();
+
+mongoose.connect(config.database,{ useNewUrlParser: true }, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('connected to database');
+  }
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(morgan('dev'));
+app.use(cors);
+
+app.get('/', (req, res, next) => {
+  res.json({
+    user: 'manish'
+  });
+});
+
+app.listen(3030, (err) => {
+  console.log('magic happens at port = ' + config.port);
+});
+

75c59cbe2a8d5e100eb595db4f9cd29e76068520
dev-sharan <sharan.manish@ymail.com>
2019-04-04 19:12:25 +0530

    review schema and deep-populate in product

--- models/product.js
+++ models/product.js
@@ -1,14 +1,19 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const deepPopulate = require('mongoose-deep-populate')(mongoose);
+
 const ProductSchema = new Schema({
   category: { type: Schema.Types.ObjectId, ref: 'Category' },
   owner: { type: Schema.Types.ObjectId, ref: 'User' },
+  reviews: [{ type: Schema.Types.ObjectId, ref: 'Review'}],
   image: String,
   title: String,
   description: String,
   price: Number,
   created: { type: Date, default: Date.now }
 });
+
+ProductSchema.plugin(deepPopulate);
 
 module.exports = mongoose.model('Product', ProductSchema);
--- routes/main.js
+++ routes/main.js
@@ -2,6 +2,9 @@ const router = require('express').Router();
 const async =  require('async');
 const Category = require('../models/category');
 const Product = require('../models/product');
+const Review = require('../models/review');
+
+const checkJWT = require('../middlewares/check-jwt');
 
 router.get('/products', (req, res, next) => {
   const perPage = 10;
@@ -81,6 +84,7 @@ router.get('/categories/:id', (req, res, next) => {
         .limit(perPage)
         .populate('category')
         .populate('owner')
+        .populate('review')
         .exec((err, products) => {
           if(err) return next(err);
           callback(err, products);
@@ -110,6 +114,7 @@ router.get('/product/:id', (req, res, next) => {
   Product.findById({ _id: req.params.id })
     .populate('category')
     .populate('owner')
+    .deepPopulate('reviews.owner')
     .exec((err, product) => {
       if(err) {
         res.json({
@@ -125,6 +130,34 @@ router.get('/product/:id', (req, res, next) => {
         }
       }
     });
+});
+
+router.post('/review', checkJWT, (req, res, next) => {
+  async.waterfall([
+    function(callback) {
+      Product.findOne({ _id: req.body.productId}, (err, product) => {
+        if (product) {
+          callback(err, product);
+        }
+      });
+    },
+    function(product) {
+      let review = new Review();
+      review.owner = req.decoded.user._id;
+
+      if(req.body.title) review.title = req.body.title;
+      if(req.body.description) review.description = req.body.description;
+      review.rating = req.body.rating;
+
+      product.reviews.push(review._id);
+      product.save();
+      review.save();
+      res.json({
+        success: true,
+        message: "Successfully added the review"
+      });
+    }
+  ]);
 });
 
 module.exports = router;

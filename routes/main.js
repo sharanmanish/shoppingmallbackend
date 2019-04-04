5d80797d460c237e8c1c5a4d2ffde7e2fbec61f8
dev-sharan <sharan.manish@ymail.com>
2019-04-04 09:48:24 +0530

    categories:/id pagination wise

--- routes/main.js
+++ routes/main.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const async =  require('async');
 const Category = require('../models/category');
 const Product = require('../models/product');
 
@@ -32,20 +33,43 @@ router.route('/categories')
 
 router.get('/categories/:id', (req, res, next) => {
   const perPage = 10;
-  Product.find({ category: req.params.id })
-    .populate('category')
-    .exec((err, products) => {
-      Product.countDocuments({ category: req.params.id }, (err, totalProducts) => {
-        res.json({
-          success: true,
-          message: 'category',
-          products: products,
-          categoryName: products[0].category.name,
-          totalProducts: totalProducts,
-          pages: Math.ceil(totalProducts / perPage)
-        });
+  const page = req.query.page;
+  async.parallel([
+    function(callback) {
+      Product.count({ category: req.params.id }, (err, count) => {
+        var totalProducts = count;
+        callback(err, totalProducts);
       });
+    },
+    function(callback) {
+      Product.find({ category: req.params.id })
+        .skip(perPage*page)
+        .limit(perPage)
+        .populate('category')
+        .populate('owner')
+        .exec((err, products) => {
+          if(err) return next(err);
+          callback(err, products);
+        });
+    },
+    function(callback) {
+      Category.findOne({ _id: req.params.id }, (err, category) => {
+        callback(err, category);
+      });
+    }
+  ], function(err, results) {
+    var totalProducts = results[0];
+    var products = results[1];
+    var category = results[2];
+    res.json({
+      success: true,
+      message: 'category',
+      products: products,
+      categoryName: category.name,
+      totalProducts: totalProducts,
+      pages: Math.ceil(totalProducts/perPage)
     });
+  });
 });
 
 module.exports = router;
